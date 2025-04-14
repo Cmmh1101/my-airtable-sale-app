9528df1e1adfd4b272e6847a27bac991db0f998f
Carla Montano <cm.emprendiendo@gmail.com>
2025-04-14 09:46:42 -0500

    clean up

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { useEffect } from "react";
-import { SlideshowLightbox, initLightboxJS } from "lightbox.js-react";
 import "lightbox.js-react";
 import Image from "next/image";
 import IProduct from "../interfaces";
@@ -11,38 +9,9 @@ interface GalleryProps {
 }
 
 const Gallery: React.FC<GalleryProps> = ({ product }) => {
-  //   useEffect(() => {
-  //     initLightboxJS(
-  //       process.env.NEXT_PUBLIC_LIGHTBOX_KEY!,
-  //       process.env.NEXT_PUBLIC_LIGHTBOX_PLAN!
-  //     );
-  //   }, []);
 
   return (
     <div className="w-full">
-      {/* <SlideshowLightbox
-        lightboxIdentifier={`lightbox-${product.id}`}
-        theme="lightbox"
-        framework="next"
-        images={product.Images?.map((img) => img.url)}
-        showThumbnails={true}
-        thumbnailBorder="silver"
-        imgAnimation="fade"
-        className="grid grid-cols-2 md:grid-cols-3 gap-2"
-      >
-        {product.Images?.map((image, i) => (
-          <div key={`${product.id}-${i}`} className="relative aspect-square overflow-hidden">
-            <Image
-              src={image.url}
-              alt={`${product.Name} ${i + 1}`}
-              fill
-              quality={80}
-              data-lightboxjs={`lightbox-${product.id}`}
-              className="object-cover rounded-lg opacity-80 hover:opacity-100 transition hover:scale-105 duration-500 ease-in-out"
-            />
-          </div>
-        ))}
-      </SlideshowLightbox> */}
       {product.Images?.map((image, i) => {
         return (
           <Image
