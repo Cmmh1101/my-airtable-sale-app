49b1cd34ce8af3f1cf205e3122039a6f0dd90b90
Carla Montano <cm.emprendiendo@gmail.com>
2025-04-22 22:20:57 -0500

    added location

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,10 +7,11 @@ const Header = () => {
   const {setSelectedProduct} = useProducts()
   return (
     <header className="dark:bg-black shadow-md py-4 px-6 sticky top-0 z-80">
-      <div className="max-w-7xl mx-auto flex justify-center items-center">
+      <div className="max-w-7xl mx-auto flex flex-col justify-center items-center">
         <Link href="/" onClick={() => setSelectedProduct(undefined)} className="text-3xl font-bold text-pink-600 tracking-wide">
           🏷️ Moving Sale
         </Link>
+        <p>Spring Hill, TN</p>
         {/* <nav className="space-x-4 text-sm font-medium">
           <Link href="/" className="hover:text-pink-600 transition">Home</Link>
           <Link href="/contact" className="hover:text-pink-600 transition">Contact</Link>
